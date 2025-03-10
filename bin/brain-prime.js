898e20a7c985aa4871899a6069d0d95edba44c55
ivanba1 <astashov.06@gmail.com>
2025-03-10 12:24:07 +0700

    Create brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { greetingMessage } from '../src/cli.js';
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function brainPrime() {
+   const name = greetingMessage();
+    console.log('Answer "yes" if the given number is prime. Otherwise answer "no".');
+
+    let correctAnswers = 0;
+    const totalQuestions = 3;
+
+    while (correctAnswers < totalQuestions) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1; 
+        const correctAnswer = isPrime(randomNumber) ? "yes" : "no";
+
+        const userAnswer = prompt(`Question: ${randomNumber}\nYour answer:`);
+
+        if (userAnswer === null) {
+            console.log("Okay, maybe next time!");
+            return;
+        }
+
+        const userAnswerLower = userAnswer.toLowerCase(); 
+
+        if (userAnswer.length === correctAnswer.length) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+}
+  
+brainPrime();
+
